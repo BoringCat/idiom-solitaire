f451354e41773e04234a51e7a620e5a8e0ad6279
boringcat <c654477757@gmail.com>
2020-02-10 20:24:52 +0800

    更改数据传输方式

--- app.py
+++ app.py
@@ -4,76 +4,103 @@ import asyncio
 import websockets
 import random
 from os import environ
-from json import dumps, dump
-import signal
+from json import dumps, loads
 from datetime import datetime
 from 成语接龙 import 成语接龙
 
-def save_history(*args):
-    global issave, maxlist
-    if issave: return
-    if not maxlist: return
-    with open('datas/max_history_%s.json' % datetime.now().strftime('%Y-%m-%d_%H:%M:%S'), 'w', encoding='UTF-8') as f:
-        dump(maxlist,f,ensure_ascii=False,separators=(',',':'))
-        f.flush()
-    issave = True
+def disconnect(msg):
+    return dumps({
+        'action': "disconnect",
+        'msg': msg
+    },ensure_ascii=False,separators=(',',':'))
+
+def connect(youfirst, output, extra):
+    return dumps({
+        'action': "connect",
+        'youfirst': youfirst,
+        'output': output,
+        'extra': extra
+    },ensure_ascii=False,separators=(',',':'))
+
+def msg(status, output, extra):
+    return dumps({
+        'action': "msg",
+        'status': status,
+        'output': output,
+        'extra': extra
+    },ensure_ascii=False,separators=(',',':'))
+
+def finish(youwin, output):
+    return dumps({
+        'action': "finish",
+        'status': youwin,
+        'output': output
+    },ensure_ascii=False,separators=(',',':'))
+
 
 async def cyjl(websocket:websockets.server.WebSocketServerProtocol, path):
     global maxnum, maxlist
     if path != globals()['path']: 
         await websocket.close_connection()
         return 
-    await websocket.send('选择模式')
-    raw = (await websocket.recv()).split(',', maxsplit=1)
-    if len(raw) == 2:
-        mode, noyd = raw
-    else:
-        noyd = False
-        mode = raw[0]
-    noyd = noyd in [True, 'True', 'true']
-    if mode not in ['拼音', '文字']:
-        await websocket.send('接龙模式 必须为\'拼音\' 或者 \'文字\'。')
-        await websocket.close(1001, "mode error")
-    jl = 成语接龙(4, noyd)
-    jl.选择配置(mode)
-    num = 0
-    userlist = []
+    await websocket.send(dumps({
+        'action': 'init'
+    }))
+    try:
+        initdict = loads(await websocket.recv())
+    except Exception:
+        await websocket.send(disconnect('出错'))
+        await websocket.close(1001, "error")
+        return
+    if initdict.get('action') != "connect":
+        await websocket.send(disconnect('数据异常'))
+        await websocket.close(1001, "action error")
+        return
+    user_mode = initdict.get('mode', None)
+    user_length = initdict.get('length', 0)
+    user_noyd = initdict.get('noyd', False)
+    try:
+        if user_mode not in ['拼音', '文字']:
+            await websocket.send(disconnect("接龙模式 必须为'拼音'或者'文字'。"))
+            await websocket.close(1001, "mode error")
+            return
+        lenlimit = int(user_length)
+        if lenlimit<0 or lenlimit>8:
+            await websocket.send(disconnect("长度限制出错"))
+            await websocket.close(1001, "limit error")
+            return
+        noyd = user_noyd in [True, 'True', 'true']
+        del user_noyd, user_length
+    except Exception:
+        await websocket.send(disconnect('数据异常'))
+        await websocket.close(1001, "action error")
+        return
+
+    jl = 成语接龙(lenlimit, noyd)
+    jl.选择配置(user_mode)
+    del lenlimit, noyd, user_mode
 
     if random.random() >= 0.6:
         status, output, extra = jl.电脑开局()
-        await websocket.send(dumps({'status': status, 'output':output, 'extra': extra},ensure_ascii=False))
-        num += 1
-        userlist.append(output)
+        await websocket.send(connect(False, output, extra))
     else:
-        await websocket.send(dumps({'status': True, 'output':None, 'extra': None},ensure_ascii=False))
+        await websocket.send(connect(True, None, None))
 
     while True:
         user_input = await websocket.recv()
         if not user_input:
+            await websocket.send(msg(False, None, False))
             await websocket.close(1000, "Finish")
-            if maxnum < num and num > 1:
-                maxnum = num
-                maxlist = userlist.copy()
             return
         status, output, extra = jl.接龙(user_input)
-        await websocket.send(dumps({'status': status, 'output':output, 'extra': extra},ensure_ascii=False))
-        if not status:
-            if extra:
-                num+=1
-                userlist.append(user_input)
+        if status:
+            await websocket.send(msg(status, output, extra))
+        else:
+            await websocket.send(finish(extra in [True,'true','True'], output))
             await websocket.close(1000, "Finish")
-            if maxnum < num and num > 1:
-                maxnum = num
-                maxlist = userlist.copy()
             return
-        if user_input!=output:
-            num += 2
-            userlist.append(user_input)
-            userlist.append(output)
 
     await websocket.close(1011, "??????")
-
-signal.signal(signal.SIGTERM, save_history)
 
 addr = environ.get('APP_LISTEN', 'localhost')
 port = int(environ.get('APP_PORT', '8765'))
@@ -88,4 +115,4 @@ asyncio.get_event_loop().run_until_complete(start_server)
 try:
     asyncio.get_event_loop().run_forever()
 except KeyboardInterrupt:
-    save_history()
+    pass
